d705738df70e425e473945762b9a5094f557d64d
Johnatan <johnatan.f@hotmail.com>
2025-08-19 23:51:09 +0200

    Atualização do Google Tag Manager para GTM-MW2HHJZ5 no head e body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,12 +121,12 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N2TBPPQ3');
-          `,
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MW2HHJZ5');
+            `,
           }}
         />
         {/* End Google Tag Manager */}
@@ -165,7 +165,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N2TBPPQ3"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MW2HHJZ5"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
