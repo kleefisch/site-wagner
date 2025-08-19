a123e759afd369dd137ce1b8deb4306e5974953f
Johnatan <johnatan.f@hotmail.com>
2025-08-19 18:20:39 +0200

    Adiciona Google Tag Manager no layout do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,6 +117,19 @@ export default function RootLayout({
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#1a1a1a" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2TBPPQ3');
+          `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Dados Estruturados Schema.org */}
         <script
           type="application/ld+json"
@@ -149,6 +162,16 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2TBPPQ3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <WhatsappPopup />
       </body>
